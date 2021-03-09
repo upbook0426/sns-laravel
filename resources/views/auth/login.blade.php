366488b4ea0b75b2b5c329e70f33fc888850805f
upbook26 <upbook.26@gmail.com>
2021-03-09 23:13:17 +0900

    初期値設定

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,12 +21,12 @@
 
                 <div class="md-form">
                   <label for="email">メールアドレス</label>
-                  <input class="form-control" type="text" id="email" name="email" required value="{{ old('email') }}">
+                  <input class="form-control" type="text" id="email" name="email" required value="guest@sample.jp"> {{--"{{ old('email') }}"--}}
                 </div>
 
                 <div class="md-form">
                   <label for="password">パスワード</label>
-                  <input class="form-control" type="password" id="password" name="password" required>
+                  <input class="form-control" type="password" id="password" name="password" required value="12345678">
                 </div>
  
                 
@@ -47,6 +47,9 @@
           </div>
         </div>
       </div>
+      
     </div>
+    <br>
+    <p style="text-align:center; color:#F00"> ↑ポートフォリオ用なので、登録済みユーザを初期値に設定しています。</p>
   </div>
 @endsection 
