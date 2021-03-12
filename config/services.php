f7366c7d7270c9e9929f5a022c0c7fc3c6c854c9
upbook26 <upbook.26@gmail.com>
2021-03-12 21:21:01 +0900

    修正

--- config/services.php
+++ config/services.php
@@ -30,8 +30,11 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
     'google' => [
-        'client_id' => 'GOOGLE_CLIENT_ID',
-        'client_secret' => 'GOOGLE_CLIENT_SECRET',
-        'redirect' => 'http://localhost/login/google/callback',
+
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('APP_URL') . '/login/google/callback',
     ],
-];
+
+    ];
+
